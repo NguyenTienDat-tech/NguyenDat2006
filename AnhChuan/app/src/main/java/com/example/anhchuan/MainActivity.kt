be85b489f3246b78de981c02a9735f74fc9e873f
NguyenTienDat-tech <khaidaikhai222@gmail.com>
2026-03-23 19:39:01 +0700

    kghg

---
+++ AnhChuan/app/src/main/java/com/example/anhchuan/MainActivity.kt
@@ -0,0 +1,108 @@
+package com.example.anhchuan
+
+import android.os.Bundle
+import android.service.voice.VoiceInteractionSession
+import android.text.method.HideReturnsTransformationMethod
+import android.text.method.PasswordTransformationMethod
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.anhchuan.databinding.ActivityMainBinding
+import android.content.Intent
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding // lateinit var: thông báo sẽ sử dụng sau
+    private var isPassword = false
+
+    override fun onCreate(savedInstanceState: Bundle?) { //onCreate: Hàm tự động chạy đầu tiên
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater) //inflate(...): Đọc file giao diện activity_main và chuyển vào binding
+        setContentView(binding.root) //lấy toàn bộ dữ liệu vừa lấy và hiển thị lên màn hình
+
+        setupListeners()
+    }
+
+    private fun setupListeners() {
+        //Nút quay lại
+        binding.Back.setOnClickListener {
+            Back()
+        }
+
+        //Xử lý nút Register
+        binding.btnRegister.setOnClickListener {
+            validateInputsAndRegister()
+        }
+
+        //Ẩn hiện password
+        binding.imageEye.setOnClickListener {
+            ViewAndHidePassword()
+        }
+
+        //Chuyển đến màn hình đăng nhập
+        binding.Login.setOnClickListener {
+            LoginScreen()
+        }
+    }
+
+    private fun Back() {
+        val intent = Intent(Intent.ACTION_MAIN) //action_main: khởi động một thứ gì đó từ điểm xuất phát
+        intent.addCategory(Intent.CATEGORY_HOME) //category_home: đi đến màn hình chính
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK //mở màn hình chính như tác vụ mới, nếu không có thì sẽ bị lỗi vì cố nhét màn hình chính vao app
+        startActivity(intent)
+    }
+
+    private fun validateInputsAndRegister() {
+        val fullName = binding.fullName.text.toString().trim()
+        val email = binding.Email.text.toString().trim()
+        val passWord = binding.textPassword.text.toString().trim()
+        val phone = binding.phonenumber.text.toString().trim()
+        val date = binding.date.text.toString().trim()
+
+        //tên hoặc email trống
+        if (fullName.isEmpty() || email.isEmpty() || passWord.isEmpty() || phone.isEmpty() || date.isEmpty()) {
+            Toast.makeText(this, "Vui lòng nhập đủ thông tin", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        //nhập không đúng email
+        if (!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            Toast.makeText(this, "Email không hợp lệ, vui lòng nhập lại", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        //nhập thành công
+        else {
+            Toast.makeText(this, "Đăng ký thành công", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
+    private fun ViewAndHidePassword() {
+        isPassword = !isPassword
+
+        if (isPassword) {
+            binding.textPassword.transformationMethod = HideReturnsTransformationMethod.getInstance() //Hiện mật khẩu
+            binding.imageEye.setImageResource(R.drawable.eyeopen) //Hiện ảnh mắt mở
+        }
+        else {
+            binding.textPassword.transformationMethod = PasswordTransformationMethod.getInstance() //Ẩn mật khẩu
+            binding.imageEye.setImageResource(R.drawable.eye) //Hiện ảnh mắt đóng
+        }
+
+        binding.textPassword.setSelection(binding.textPassword.length()) //Đưa con trỏ vào cuối đoạn chữ
+    }
+
+    private fun LoginScreen()
+    {
+        val intent = Intent(this, SignUpActivity::class.java) //Tạo Intent để chuyển từ màn hình hiện tại sang màn hình khac
+        startActivity(intent)
+    }
+
+
+
+}
---
+++ AnhChuan/app/src/main/java/com/example/anhchuan/SignUpActivity.kt
@@ -0,0 +1,101 @@
+package com.example.anhchuan
+
+import android.content.Intent
+import android.os.Bundle
+import android.text.method.HideReturnsTransformationMethod
+import android.text.method.PasswordTransformationMethod
+import android.text.method.TransformationMethod
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.anhchuan.databinding.ActivitySignUpBinding
+
+class SignUpActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySignUpBinding
+    private var isEye = false
+    private var isSquare = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        SetUpListeners()
+
+    }
+
+    private fun SetUpListeners() {
+        binding.Login.setOnClickListener {
+            InputEmail()
+        }
+
+        binding.imageEye.setOnClickListener {
+            ShowHideEye()
+        }
+
+        binding.signUp.setOnClickListener {
+            signUpScreen()
+        }
+
+        binding.square.setOnClickListener {
+            ClickSquare()
+        }
+    }
+
+    private fun InputEmail() {
+        val email = binding.EmailText.text.toString().trim()
+        val passWord = binding.passwordText.text.toString().trim()
+
+        if (email.isEmpty() || passWord.isEmpty()) {
+            Toast.makeText(this, "Vui lòng nhập đầy đủ thông tin", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        if (!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            Toast.makeText(this, "Email không hợp lệ, vui lòng nhập lại", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        else {
+            Toast.makeText(this, "Đăng nhập thành công", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun ShowHideEye() {
+        isEye = !isEye
+
+        if (isEye) {
+            binding.passwordText.transformationMethod = HideReturnsTransformationMethod.getInstance()
+            binding.imageEye.setImageResource(R.drawable.eyeopen)
+        }
+        else {
+            binding.passwordText.transformationMethod = PasswordTransformationMethod.getInstance()
+            binding.imageEye.setImageResource(R.drawable.eye)
+        }
+
+        binding.passwordText.setSelection(binding.passwordText.length())
+    }
+
+    private fun signUpScreen() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun ClickSquare() {
+        isSquare = !isSquare
+
+        if (isSquare) {
+            binding.square.setImageResource(R.drawable.squaretrue)
+        }
+        else {
+            binding.square.setImageResource(R.drawable.square)
+        }
+    }
+
+
+}
