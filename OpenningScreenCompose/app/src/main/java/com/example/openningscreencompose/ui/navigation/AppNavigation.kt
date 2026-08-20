3aa6ab66f01fb0fefa687cced25131ffd7bdf573
NguyenTienDat-tech <khaidaikhai222@gmail.com>
2026-08-20 20:50:06 +0700

    feat: setup navigation and implement welcome screen

---
+++ OpenningScreenCompose/app/src/main/java/com/example/openningscreencompose/ui/navigation/AppNavigation.kt
@@ -0,0 +1,47 @@
+package com.example.openningscreencompose.ui.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.rememberNavController
+import com.example.openningscreencompose.ui.screens.auth.welcome.WelcomeScreen
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.example.openningscreencompose.ui.screens.auth.login.LoginScreen
+import com.example.openningscreencompose.ui.screens.auth.register.RegisterScreen
+
+@Composable
+fun AppNavigation() {
+    // 1. Tạo một bộ điều khiển (NavController) để ghi nhớ lịch sử chuyển trang
+    val navController = rememberNavController()
+
+    // 2. Tạo bản đồ điều hướng (NavHost), thiết lập màn hình bắt đầu là Welcome
+    NavHost(
+        navController = navController,
+        startDestination = Screen.Welcome.route
+    ) {
+        composable(route = Screen.Welcome.route) {
+            WelcomeScreen(
+                onNavigationToLogin = {
+                    navController.navigate(Screen.Login.route)
+                },
+
+                onNavigationToRegister = {
+                    navController.navigate(Screen.Login.route)
+                }
+            )
+        }
+
+        composable(route = Screen.Login.route) {
+            LoginScreen(
+                onNavigationToRegister = {
+                    navController.navigate(Screen.Welcome.route)
+                }
+            )
+        }
+
+        composable(route = Screen.Register.route) {
+            RegisterScreen(
+
+            )
+        }
+    }
+}
---
+++ OpenningScreenCompose/app/src/main/java/com/example/openningscreencompose/ui/screens/auth/welcome/WelcomeViewModel.kt
@@ -0,0 +1,4 @@
+package com.example.openningscreencompose.ui.screens.auth.welcome
+
+class WelcomeViewModel {
+}
